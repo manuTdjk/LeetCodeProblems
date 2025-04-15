47c9aea846642b325e5d4098e3eebaa9994fd662
manuTdjk <113052669+manuTdjk@users.noreply.github.com>
2025-04-15 15:30:55 +0530

    Update SetZeros.java

--- SetZeros.java
+++ SetZeros.java
@@ -24,3 +24,30 @@ class setZeros {
 
     }
 }
+//another Soln
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+        
+//         Set<Integer> columnZero=new HashSet<>();
+//         Set<Integer> rowZero=new HashSet<>();
+
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[i].length;j++){
+//                 if(matrix[i][j]==0){
+//                     rowZero.add(i);
+//                     columnZero.add(j);
+//                 }
+//             }
+//         }
+
+//         for(int i=0;i<matrix.length;i++){
+//             if(rowZero.contains(i))Arrays.fill(matrix[i],0);
+//             for(int j=0;j<matrix[i].length;j++){
+//                 if(columnZero.contains(j)){
+//                     matrix[i][j]=0;
+//                 }
+//             }
+//         }
+
+//     }
+// }
